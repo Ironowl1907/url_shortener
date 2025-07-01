e2e58b1f1ce712b46db65440d06de35904d608e6
Facundo <facuguialtana7@gmail.com>
2025-07-01 13:41:18 -0300

    db connection working

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,20 +1,16 @@
 package db
 
 import (
-	"github.com/ironowl1097/url_shortener/internal/db/models"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=localhost user=... password=... dbname=... port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=verySecurePassword dbname=url_shortener port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
 	}
-
-	db.AutoMigrate(&models.User{}, &models.ShortenedURL{})
 
 	return db
 }
