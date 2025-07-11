32ec7d04397cc1e3a29cfdc17ac71fd26eb22745
Facundo <facuguialtana7@gmail.com>
2025-07-11 10:14:38 -0300

    Working with .env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ironowl1907/url_shortener/internal/app"
+	"github.com/ironowl1907/url_shortener/internal/tools"
 )
 
 func main() {
+	// Load .env file
+	{
+		err := tools.LoadEnvVariables()
+		if err != nil {
+			log.Panic("error loading .env file: " + err.Error())
+		}
+	}
+
+	// Create new app
 	app := app.NewApp()
 
+	// Initialize the DB
 	if err := app.InitDB(); err != nil {
 		panic("Couldn't connect to database")
 	}
+	// Setup the routes
 	app.SetupRoutes()
 
-	if err := app.Run("8080"); err != nil {
+	// Run the server
+	if err := app.Run(os.Getenv("PORT")); err != nil {
 		panic("Couldn't run server")
 	}
 }
