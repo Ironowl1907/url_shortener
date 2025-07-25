e40d030bfa5ae0d81a8461c2fe42295de798be83
Facundo <facuguialtana7@gmail.com>
2025-07-25 13:13:12 -0300

    Simple nav bar

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,39 @@
-<script>
+<script lang="ts">
 </script>
 
-<nav>
-	<h1></h1>
+<nav class="navbar">
+	<div class="brand">
+		<a href="/">URL Shortener</a>
+	</div>
+
+	<div class="nav-links">
+		<a href="/">Home</a>
+		<a href="/login">Login</a>
+		<a href="/register">Register</a>
+	</div>
 </nav>
+
+<style>
+	.navbar {
+		color: white;
+		padding: 1rem 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.brand {
+		font-size: 1.25rem;
+		font-weight: bold;
+	}
+
+	.nav-links a {
+		margin-left: 1rem;
+		text-decoration: none;
+		color: black;
+	}
+
+	.nav-links a:hover {
+		text-decoration: underline;
+	}
+</style>
